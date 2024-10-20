2629763cab7d6c30bbabb5ea19ce22903c0a5eca
cauanvmb <cauanvmb15@gmail.com>
2024-10-20 00:04:48 -0300

    :sparkles: solved the conflict situation in the "Tooltip" branch

--- src/components/BaseMap.vue
+++ src/components/BaseMap.vue
@@ -1,89 +1,55 @@
 <template>
   <div class="map-wrapper">
-    <GeoFilterView class="filter-overlay" @saveFilter="handleFilterData" @clearPoints="clearPoints"/>
-    <div v-if="showPlayback" class="playback-layer">
-      <PlaybackControl
-        v-model:rota="route" 
-        v-model:iconMap="startPointIconMap"
-        v-model:allCoordinatesAnimation="allCoordinatesAnimation"
-        v-model:anguloInicial="anguloInicial"
-      />
-    </div>
+    <GeoFilterView class="filter-overlay" @saveFilter="handleFilterData"></GeoFilterView>
     <div id="map" class="map-container"></div>
+    <div id="popup" class="ol-popup">
+      <div id="popup-content"></div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
-import { Map, View, Feature } from 'ol';
+import { Map, View, Feature, Overlay } from 'ol';
 import { Tile as TileLayer } from 'ol/layer';
-import { OSM, XYZ } from 'ol/source';
+import { OSM } from 'ol/source';
 import { Vector as VectorLayer } from 'ol/layer';
 import { Vector as VectorSource } from 'ol/source';
-import { Point, LineString, Geometry } from 'ol/geom';
+import { Point, LineString } from 'ol/geom';
 import { Icon, Style, Stroke } from 'ol/style';
 import axios from 'axios';
 import IconStartPin from '../assets/IconStartPin.png';
-import IconStartAndEnd from '../assets/IconStartAndEnd.png';
 import IconEndPin from '../assets/IconEndPin.png';
 import GeoFilterView from "@/views/GeoFilterView.vue";
-import IconPositionMap from '../assets/IconPositionMap.png';
-import PlaybackControl from '@/views/PlaybackControl.vue';
-import type { Coordinate } from 'ol/coordinate';
 import {useToast} from "vue-toastification";
-import { boundingExtent } from 'ol/extent';
-import {error} from "ol/console";
+import {boundingExtent} from "ol/extent";
+import {fetchPersonById, fetchPersons} from "@/services/apiService.js";
 
 const toast = useToast();
 
-let center = ref([-60.457873,0.584053]);
 let projection = ref("EPSG:4326");
+let center = ref([-60.457873, 0.584053]);
 let zoom = ref(5);
 let map = ref<Map | null>(null);
 let pointFeatures = ref<Feature[]>([]);
 let routeLine = ref<Feature[]>([]);
 let pointFinalStar = ref<Feature[]>([]);
 let lineLayer = ref<VectorLayer<VectorSource> | null>(null);
-let anguloInicial = 0;
+let thePerson:{person: number | null, startDate:string | null, endDate:string | null};
+let PersonOption = ref<{ label: string; value: number }[]>([]);
 
-const startPointIconMap = ref<Feature<Geometry>>();
-const route = ref<LineString>();
-const allCoordinatesAnimation = ref<Coordinate[]>([]);
-const showPlayback = ref(false);
 
-function getInitialRotation() {
-  const [lon1, lat1] = allCoordinatesAnimation.value[0];
-  const [lon2, lat2] = allCoordinatesAnimation.value[1];
-
-  const deltaLon = lon2 - lon1;
-  const deltaLat = lat2 - lat1;
-
-  anguloInicial = Math.atan2(deltaLat, deltaLon)*-1;
-}
-
-function handleFilterData(filterData:{person: number | undefined, startDate:string | null, endDate:string | null}){
+function handleFilterData(filterData:{person: number | null, startDate:string | null, endDate:string | null}){
   pointFeatures.value = [];
-  if (map.value) {
-    map.value.values_.layergroup.values_.layers.array_.forEach((layer) => {
-      map.value.values_.layergroup.values_.layers.array_.pop(layer);
-    })
-  }
-  const baseLayer = new TileLayer({
-    source: new XYZ({
-      url: `https://api.maptiler.com/maps/dataviz-dark/{z}/{x}/{y}.png?key=DxUujwebq5Zd8hO25SyJ`
-    }),
-  });
-  map.value.addLayer(baseLayer);
+  map.value.removeLayers;
   routeLine.value = [];
   pointFinalStar.value = [];
 
   let getUrl = `http://localhost:8080/tracker/period/${filterData.person}/${filterData.startDate}T00:00:00.000/${filterData.endDate}T00:00:00.000?page=0`;
 
   getAllPoints(getUrl).then((points) => {
-    if (!points) {
-      if (showPlayback.value) {
-        showPlayback.value = false;
-      }
+    if (points.length === 0) {
+      toast.info("Nenhum ponto encontrado para o filtro selecionado.");
     } else {
       let pointList = new ref(points);
       makeGeometryPointFromArray(pointList, filterData.person);
@@ -94,45 +60,15 @@ function handleFilterData(filterData:{person: number | undefined, startDate:stri
   });
 }
 
-function clearPoints() {
-  if (map.value) {
-    map.value.values_.layergroup.values_.layers.array_.forEach((layer) => {
-      map.value.values_.layergroup.values_.layers.array_.pop(layer);
-    })
-    pointFeatures.value = [];
-    routeLine.value = [];
-    pointFinalStar.value = [];
-
-    const baseLayer = new TileLayer({
-          source: new XYZ({
-            url: `https://api.maptiler.com/maps/dataviz-dark/{z}/{x}/{y}.png?key=DxUujwebq5Zd8hO25SyJ`
-          }),
-    });
-    map.value.addLayer(baseLayer);
-    adjustMap();
-  }
-}
-
 const getAllPoints = async (getPointsUrl: string) => {
-  const response = await axios.get(getPointsUrl) .catch(function (error) {
-      if(error.response) {
-          toast.info("Nenhum ponto encontrado para o filtro selecionado.");
-        if (axios.isAxiosError(error) && error.response) {
-          const errorMessage = error.response.data?.message ||
-              "Erro desconhecido ao buscar pontos.";
-        } else {
-          console.error(error);
-          toast.error("Erro na conexão. Tente novamente mais tarde.");
-        }
-        return [];
-      }
+  try {
+    const response = await axios.get(getPointsUrl);
     return response.data.content;
-  })
+  } catch (error) {
+    console.error(error);
+    toast.error("Erro ao buscar pontos. Tente novamente mais tarde.");
   }
-
-
-
-
+};
 
 function createStartLayer(pointFinalStarArrayOfFeatures) {
   const vectorLayer = new VectorLayer({
@@ -148,33 +84,26 @@ function makeGeometryPointFromArray(arrayOfGeometryObjects, nameFilter?) {
   if (arrayOfGeometryObjects.length === 0) return [];
 
   if (nameFilter) {
-    const startPointStartPin = new Feature({
+    const startPoint = new Feature({
       geometry: new Point([arrayOfGeometryObjects.value[0].longitude, arrayOfGeometryObjects.value[0].latitude]),
+      name: 'Início do percurso' // Adiciona o title nativo
     });
 
-    startPointStartPin.setStyle(new Style({
+    startPoint.setStyle(new Style({
       image: new Icon({
         src: IconStartPin,
         scale: 0.7,
         anchor: [0.5, 1],
       }),
     }));
+    startPoint.setProperties({
+      personName: nameFilter
+    })
 
-    startPointIconMap.value = new Feature({
-      geometry: new Point([arrayOfGeometryObjects.value[0].longitude, arrayOfGeometryObjects.value[0].latitude]),
-    });
-
-    startPointIconMap.value.setStyle(new Style({
-      image: new Icon({
-        src: IconPositionMap,
-        anchor: [0.5, 0.5],
-        scale: 0.2,
-        rotation: anguloInicial
-      }),
-    }));
-    
     const endPoint = new Feature({
       geometry: new Point([arrayOfGeometryObjects.value[arrayOfGeometryObjects.value.length - 1].longitude, arrayOfGeometryObjects.value[arrayOfGeometryObjects.value.length - 1].latitude]),
+      name: 'Fim do percurso' // Adiciona o title nativo
+
     });
 
     endPoint.setStyle(new Style({
@@ -184,42 +113,14 @@ function makeGeometryPointFromArray(arrayOfGeometryObjects, nameFilter?) {
         anchor: [0.5, 1],
       }),
     }));
+    endPoint.setProperties({
+      personName: nameFilter
+    })
 
-    if(startPointStartPin.getGeometry()?.getCoordinates()[0] === endPoint.getGeometry()?.getCoordinates()[0]){
-      if (showPlayback.value) {
-        showPlayback.value = false;
-      }
-      toast.info("A localização do ponto de início é o mesmo do ponto de fim.");
+    pointFinalStar.value.push(startPoint);
+    pointFinalStar.value.push(endPoint);
+    createStartLayer(pointFinalStar);
 
-      const startAndEnd = new Feature({
-        geometry: new Point([arrayOfGeometryObjects.value[0].longitude, arrayOfGeometryObjects.value[0].latitude]),
-      });
-
-      startAndEnd.setStyle(new Style({
-        image: new Icon({
-          src: IconStartAndEnd,
-          scale: 0.7,
-          anchor: [0.5, 1],
-        })
-      }));
-      pointFinalStar.value.push(startAndEnd);
-      createStartLayer(pointFinalStar);
-    } else {
-        pointFinalStar.value.push(startPointStartPin);
-        pointFinalStar.value.push(startPointIconMap.value);
-        pointFinalStar.value.push(endPoint);
-        createStartLayer(pointFinalStar);
-      center.value = endPoint.getGeometry().getCoordinates();
-
-      if (!showPlayback.value) {
-        showPlayback.value = !showPlayback.value;
-      } else {
-        showPlayback.value = false;
-        setTimeout(() => {
-          showPlayback.value = true;
-        }, 1);
-      }
-    }
     center.value = endPoint.getGeometry().getCoordinates();
   }
 
@@ -239,7 +140,8 @@ function makeGeometryPointFromArray(arrayOfGeometryObjects, nameFilter?) {
 }
 
 function makeLineFromPoints(featureList) {
-  if (!featureList) {
+  if (featureList.value.length === 0) {
+    console.log('Nenhum ponto disponível para criar linhas');
     toast.info("Nenhum ponto disponível para criar linhas.");
     return null;
   }
@@ -253,33 +155,24 @@ function makeLineFromPoints(featureList) {
 
   Object.keys(groupedById).forEach((idText) => {
     const points = groupedById[idText];
-    allCoordinatesAnimation.value = [];
-
     if (points.length >= 2) {
       for (let i = 0; i < points.length - 1; i++) {
         const point1 = points[i];
         const point2 = points[i + 1];
-  
-        allCoordinatesAnimation.value.push(point1.getGeometry().getCoordinates())
-        allCoordinatesAnimation.value.push(point2.getGeometry().getCoordinates())
+
+        const lineFeature = new Feature({
+          geometry: new LineString([point1.getGeometry().getCoordinates(), point2.getGeometry().getCoordinates()]),
+        });
+        lineFeature.setStyle(new Style({
+          stroke: new Stroke({
+            color: '#ec1c24',
+            width: 6,
+          }),
+        }));
+        routeLine.value.push(lineFeature);
       }
-      
-      route.value = new LineString(allCoordinatesAnimation.value)
-
-      getInitialRotation();
-
-      const lineFeature = new Feature({
-        geometry: route.value,
-      });
-      lineFeature.setStyle(new Style({
-        stroke: new Stroke({
-          color: '#ec1c24',
-          width: 6,
-        }),
-      }));
-      routeLine.value.push(lineFeature);
     }
-  })
+  });
   return new VectorLayer({
     source: new VectorSource({
       features: routeLine.value,
@@ -304,9 +197,7 @@ const createMap = () => {
     target: 'map',
     layers: [
       new TileLayer({
-        source: new XYZ({
-          url: `https://api.maptiler.com/maps/dataviz-dark/{z}/{x}/{y}.png?key=DxUujwebq5Zd8hO25SyJ`
-        }),
+        source: new OSM(),
       }),
     ],
     view: new View({
@@ -331,46 +222,108 @@ const createMap = () => {
   map.value.addLayer(vectorLayer);
   map.value.addLayer(routeLayer);
 }
+
+async function getPersonName(personID: number): Promise<{ label: string; value: number } | undefined> {
+  let personListFromDb = await fetchPersons();
+
+  
+  // Atribui a lista de opções com a tipagem correta
+  PersonOption.value = personListFromDb.map(person => ({
+    label: person.fullName.toUpperCase(),
+    value: person.idPerson
+  }));
+
+  // Retorna a pessoa correspondente ao ID
+  return PersonOption.value.find(person => person.value.idPerson === personID);
+}
+
 onMounted(() => {
-  createMap();
+
+  createMap()
+  
+  const popupElement = document.getElementById('popup') as HTMLElement;
+  const popupContent = document.getElementById('popup-content') as HTMLElement;
+  const popupOverlay = new Overlay({
+    element: popupElement,
+    autoPan: true,
+    autoPanAnimation: {
+      duration: 250,
+    },
+  });
+  map.value?.addOverlay(popupOverlay);
+  
+  // Adiciona o evento de clique ao mapa
+  map.value?.on('click', (e) => {
+  let clickedFeature = false;
+
+  map.value?.forEachFeatureAtPixel(e.pixel, (feature) => {
+    const featureName = feature.get('name');
+
+    if (featureName) {
+      clickedFeature = true;
+      const coordinates = feature.getGeometry().getCoordinates();
+      const offsetCoordinates = [coordinates[0], coordinates[1]];
+      popupOverlay.setPosition(offsetCoordinates);
+
+      // Obtenha o nome da pessoa usando a Promise corretamente
+      getPersonName(thePerson)
+        .then(personName => {
+          // Verifique se a pessoa foi encontrada e atualize o conteúdo do popup
+          popupContent.innerHTML = `<p>${personName?.label}</p>`;
+          popupElement.style.display = 'block';
+        })
+        .catch(error => {
+          console.error(error);
+          popupContent.innerHTML = '<p>Erro ao buscar o nome</p>';
+          popupElement.style.display = 'block';
+        });
+    }
+  });
+
+  if (!clickedFeature) {
+    popupOverlay.setPosition(undefined);
+    popupElement.style.display = 'none';
+  }
 });
+});
+
+
+
 </script>
 
 <style scoped>
 .map-container {
   width: 100vw;
   height: 100vh;
+  position: relative;
+  z-index: 1;
 }
-
 .filter-overlay {
   position: absolute;
   top: 20px;
   left: 20px;
+  z-index: 10;
   background-color: white;
   padding: 10px;
   border-radius: 8px;
-  z-index: 3;
 }
-
-.playback-layer {
+.ol-popup {
   position: absolute;
-  width: 100%;
-  height: 6.8%;
-  bottom: 0px;
-  z-index: 2;
-  transition: bottom 0.5s ease;
+  background-color: rgb(80, 80, 80);
+  padding: 10px;
+  border: 1px solid black;
+  border-radius: 8px;
+  min-width: 150px;
+  box-shadow: 0 1px 4px rgba(0, 0, 0, 0.2);
+  display: none;
+  pointer-events: none; /* Desativa interações quando o popup está escondido */
 }
 
-:global(.ol-zoom-in) {
-  bottom: 6em;
-  right: 2em;
-  position: fixed;
+.ol-popup.active {
+  display: block;
+  pointer-events: auto; /* Ativa interações quando o popup está visível */
 }
 
-:global(.ol-zoom-out) {
-  bottom: 4.5em;
-  right: 2em;
-  position: fixed;
-}
+
 
 </style>
